58150f513f3a20c13e747e41708cb6c3bd06c79d
Daniel Fildán <fildand@centrum.cz>
2025-07-05 21:58:35 +0200

    Fix a crash when the window was set to maximized

--- src/main/java/net/danygames2014/gambac/mixin/MinecraftAppletMixin.java
+++ src/main/java/net/danygames2014/gambac/mixin/MinecraftAppletMixin.java
@@ -20,32 +20,39 @@ import java.awt.*;
 @Mixin(MinecraftApplet.class)
 public class MinecraftAppletMixin extends Applet {
 
-    @Shadow private Minecraft minecraft;
+    @Shadow
+    private Minecraft minecraft;
 
     /**
      * @author Proudly overwritten by DanyGames2014
      * @reason because i don't give a shit
      */
     @Overwrite(remap = false)
-    public void init(){
+    public void init() {
         //System.out.println("Properties:");
         //System.getProperties().forEach( (k, v) -> System.out.println(k + " : " + v));
-         
+
         // PrismLauncher Window Size
-        if(System.getProperty("org.prismlauncher.window.dimensions") != null) {
+        if (System.getProperty("org.prismlauncher.window.dimensions") != null) {
             String[] dimensions = System.getProperty("org.prismlauncher.window.dimensions").split("x");
-            if(dimensions.length == 2) {
-                this.setSize(Integer.parseInt(dimensions[0]), Integer.parseInt(dimensions[1]));
+            if (dimensions.length == 2) {
+                try {
+                    int prismWidth = Integer.parseInt(dimensions[0]);
+                    int prismHeight = Integer.parseInt(dimensions[1]);
+                    this.setSize(prismWidth, prismHeight);
+                } catch (NumberFormatException ignored) {
+
+                }
             }
         }
-        
+
         boolean fullscreen = false;
         if (this.getParameter("fullscreen") != null) {
             fullscreen = this.getParameter("fullscreen").equalsIgnoreCase("true");
         }
-        
+
         this.minecraft = new BrnoMinecraft(this.getWidth(), this.getHeight(), fullscreen);
-        
+
         this.minecraft.hostAddress = this.getDocumentBase().getHost();
         if (this.getDocumentBase().getPort() > 0) {
             StringBuilder hostAdressBuilder = new StringBuilder();
@@ -68,7 +75,7 @@ public class MinecraftAppletMixin extends Applet {
         }
 
         this.startThread();
-        
+
         SwingUtilities.invokeLater(() -> {
             hideThemAll(this.getParent().getParent().getParent());
             hideThemAll(this.getParent().getParent());
@@ -104,7 +111,7 @@ public class MinecraftAppletMixin extends Applet {
         ci.cancel();
     }
 
-    @Inject(method = "stopThread", at= @At(value = "HEAD"), cancellable = true)
+    @Inject(method = "stopThread", at = @At(value = "HEAD"), cancellable = true)
     public void stopThread(CallbackInfo ci) {
         ci.cancel();
     }
