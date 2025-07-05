6c6b3dd8f2681d7ecdbcdfb59e15dbf8f2aae2a2
Daniel Fildán <fildand@centrum.cz>
2025-07-05 21:24:25 +0200

    Well that didnt work

--- src/main/java/net/danygames2014/gambac/mixin/MinecraftMixin.java
+++ src/main/java/net/danygames2014/gambac/mixin/MinecraftMixin.java
@@ -1,27 +1,15 @@
 package net.danygames2014.gambac.mixin;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.Mouse;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin {
-
-    @Shadow
-    public boolean focused;
-
-    @Shadow
-    public abstract void pauseGame();
-
-    @Shadow
-    public Mouse mouse;
-
     @Inject(method = "init", at = @At(value = "TAIL"))
     public void makeDisplayCurrent(CallbackInfo ci) {
         try {
@@ -33,15 +21,6 @@ public abstract class MinecraftMixin {
             e.printStackTrace();
         }
     }
-
-    @Inject(method = "tick", at = @At(value = "HEAD"))
-    public void aVoid(CallbackInfo ci) {
-        if (!focused) {
-            this.pauseGame();
-            this.mouse.unlockCursor();
-        }
-    }
-
 
 // Testing of a bug that made 2 windows exist at the same time if the game crashed on startup
 //    @Inject(method = "init", at = @At(value = "TAIL"))
